1aa400141cab2589d88081ae2e6f7ace084e37af
jcrump88 <jcrump88@gmail.com>
2021-11-01 15:39:32 -0500

    updated startup.cs

--- API/Startup.cs
+++ API/Startup.cs
@@ -6,12 +6,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Persistence;
+using Microsoft.EntityFrameworkCore;
 
 namespace API
 {
@@ -23,7 +23,6 @@ namespace API
         }
 
         private readonly string CorsPolicy = "CorsPolicy";
-
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
@@ -56,10 +55,10 @@ namespace API
             else
             {
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                //app.UseHsts();
+                // app.UseHsts();
             }
 
-            //app.UseHttpsRedirection();
+            // app.UseHttpsRedirection();
             app.UseCors(CorsPolicy);
             app.UseMvc();
         }
